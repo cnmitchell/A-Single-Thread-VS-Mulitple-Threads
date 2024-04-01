fcb876321005a5933508636e67a2802e2fbde3e8
cnmitchell <56087181+cnmitchell@users.noreply.github.com>
2024-04-01 11:18:31 -0400

    Update PrimeChecker.java

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,9 +1,4 @@
-//created a class for checking for prime numbers
-//this allows me to extend it in my single/multithreaded class and use multiple times
-//without repetition of code (Respecting the rules of DRY!!)
 public class PrimeChecker {
-    //fixed isPrime logic thanks to your help
-    //followed your suggestion to use a boolean instead of divisors
     public boolean isPrime(int num){
         if(num <= 1)
             return false;
